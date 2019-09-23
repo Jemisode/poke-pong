35db2c93337124bebc4dbd9f698c5805456d7f22
Jess <jessmatthews@live.co.uk>
2019-09-23 18:45:19 +0100

    added Header component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+ import Header from './Header/Header';
+//import Players from './Players/Players';
+
+const App = () => (
+  
+   <>
+      <Header />
+   </>
+);
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+    <div>
+        <header>
+            <h1>Poké Pong</h1>
+        </header>
+    </div>
+);
+
+export default Header;
